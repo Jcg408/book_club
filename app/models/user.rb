91750b654a68b915a1aec885020d38d9d502b1ac
Jcg408 <jcg321@comcast.net>
2019-12-14 22:45:09 -0500

    added validations to user model to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :email, presence: true
+    validates :password_digest, presence: true
 end
